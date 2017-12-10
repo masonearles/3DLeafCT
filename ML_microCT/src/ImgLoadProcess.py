3caedca72e65b780455999c6b5a98dae06472c73
mattjenkins3 <mrjenkins1012@gmail.com>
2017-12-10 14:43:58 -0800

    updating

--- ML_microCT/src/ImgLoadProcess.py
+++ ML_microCT/src/ImgLoadProcess.py
@@ -19,6 +19,7 @@ from sklearn.metrics import confusion_matrix
 def Load_images(fp,gr_name,pr_name,ls_name):
     # Set path to tiff stacks
     #filepath = '../images/'
+    print("***LOADING IMAGE STACKS***")
     # Read gridrec, phaserec, and label tif stacks
     gridrec_stack = io.imread(fp + gr_name)
     phaserec_stack = io.imread(fp + pr_name)
@@ -31,6 +32,7 @@ def Load_images(fp,gr_name,pr_name,ls_name):
 
 # Threshold grid and phase images and add the IAS together, invert, downsample and save as .tif stack
 def Threshold_GridPhase_invert_down(grid_img, phase_img, Th_grid, Th_phase):
+    print("***THRESHOLDING IMAGES***")
     tmp = np.zeros(grid_img.shape)
     tmp[grid_img < Th_grid] = 1
     tmp[grid_img >= Th_grid] = 0
@@ -39,17 +41,20 @@ def Threshold_GridPhase_invert_down(grid_img, phase_img, Th_grid, Th_phase):
     tmp_invert = invert(tmp)
     #downsample to 25%
     tmp_invert_ds = transform.rescale(tmp_invert, 0.25)
+    print("***SAVING IMAGE STACK***")
     #write as a .tif file un our images folder
     io.imsave('../images/GridPhase_invert_ds.tif',tmp_invert_ds)
 
 # run local thickness, upsample and save as a .tif stack in images folder
 def localthick_up_save():
+    print("***GENERATING LOCAL THICKNESS STACK***")
     #load thresholded binary downsampled images for local thickness
     GridPhase_invert_ds = io.imread('../images/GridPhase_invert_ds.tif')
     #run local thickness
     local_thick = RFLeafSeg.local_thickness(GridPhase_invert_ds)
     #upsample local_thickness images
     local_thick_upscale = transform.rescale(local_thick, 4, mode='reflect')
+    print("***SAVING LOCAL THICKNESS STACK***")
     #write as a .tif file in our images folder
     io.imsave('../images/local_thick_upscale.tif', local_thick_upscale)
 
@@ -76,17 +81,19 @@ def displayImages_displayDims(gr_s,pr_s,ls,lt_s,gp_train,gp_test,label_train,lab
     print(lt_s.shape)
 
 def train_model(gr_s,pr_s,ls,lt_s,gp_train,gp_test,label_train,label_test):
+    print("***GENERATING FEATURE LAYERS***")
     #generate training and testing feature layer array
     FL_train_transverse = RFLeafSeg.GenerateFL2(gr_s, pr_s, lt_s, gp_train, "transverse")
     FL_test_transverse = RFLeafSeg.GenerateFL2(gr_s, pr_s, lt_s, gp_test, "transverse")
     # Load and encode label image vectors
-    Label_train_transverse = RFLeafSeg.LoadLabelData(ls, label_train, "transverse")
-    Label_test_transverse = RFLeafSeg.LoadLabelData(ls, label_test, "transverse")
+    Label_train = RFLeafSeg.LoadLabelData(ls, label_train, "transverse")
+    Label_test = RFLeafSeg.LoadLabelData(ls, label_test, "transverse")
+    print("***TRAINING MODEL***")
     # Define Random Forest classifier parameters and fit model
     rf_trans = RandomForestClassifier(n_estimators=50, verbose=True, oob_score=True, n_jobs=4, warm_start=False) #, class_weight="balanced")
-    rf_trans = rf_trans.fit(FL_train_transverse, Label_train_transverse)
+    rf_trans = rf_trans.fit(FL_train_transverse, Label_train)
 
-    return rf_trans,FL_train_transverse,FL_test_transverse
+    return rf_trans,FL_train_transverse,FL_test_transverse, Label_train, Label_test
 
 def print_feature_layers(rf_t):
     # Print feature layer importance
@@ -95,9 +102,18 @@ def print_feature_layers(rf_t):
     for fl, imp in zip(feature_layers, rf_t.feature_importances_):
         print('Feature_layer {fl} importance: {imp}'.format(fl=fl, imp=imp))
 
-def predict_testset(rf_t,FL_train,FL_test): #predict single slices from dataset
+def predict_testset(rf_t,FL_test): #predict single slices from dataset
     # Make prediction on test set
+    print("***GENERATING PREDICTED STACK***")
     class_prediction = rf_t.predict(FL_test)
     class_prediction_prob = rf_t.predict_proba(FL_test)
 
     return class_prediction, class_prediction_prob
+
+def make_conf_matrix(L_test,class_p):
+    # Generate confusion matrix for transverse section
+    pd.crosstab(L_test, class_p, rownames=['Actual'], colnames=['Predicted'])
+
+def make_normconf_matrix(L_test,class_p):
+    # Generate normalized confusion matrix for transverse section
+    pd.crosstab(L_test, class_p, rownames=['Actual'], colnames=['Predicted'], normalize='index')
--- ML_microCT/src/main.py
+++ ML_microCT/src/main.py
@@ -6,8 +6,12 @@ import skimage.io as io
 
 #Uncomment if you relocate all RFLeafSeg and other functions into this file....consolidate
 #if __name__=="__main__":
+#get terminal size for printing centered text
+#width = os.get_terminal_size().columns DEPRECATED IN os version 3.3...find new function
+
 selection = "1"
 while selection != "7":
+    print("********_____MAIN MENU_____********")
     print("1. Image loading and pre-processing")
     print("2. Train model")
     print("3. Examine prediction metrics on training dataset")
@@ -20,6 +24,7 @@ while selection != "7":
     if selection=="1": #image loading and pre-processing
         selection2 = "1"
         while selection2 != "5":
+            print("********_____IMAGE LOADING AND PRE-PROCESSING MENU_____********")
             print("1. Load image stacks")
             print("2. Generate binary threshold image, invert, downsample and save as a .tif file for input into local thickness function.")
             print("3. Run local thickness algorithm on downsampled tif file, upsample and save as a .tif file.")
@@ -45,6 +50,7 @@ while selection != "7":
             elif selection2=="3": #run local thickness, upsample, save
                 ImgLoadProcess.localthick_up_save()
             elif selection2=="4": #load processed local thickness stack and match array dimensions
+                print("***LOADING LOCAL THICKNESS STACK***")
                 localthick_stack = io.imread('../images/local_thick_upscale.tif')
                 # Match array dimensions to correct for resolution loss due to downsampling when generating local thickness
                 gridrec_stack, localthick_stack = RFLeafSeg.match_array_dim(gridrec_stack,localthick_stack)
@@ -58,12 +64,14 @@ while selection != "7":
     elif selection=="2": #train model
         selection3="1"
         while selection3 != "4":
+            print("********_____TRAIN MODEL MENU_____********")
             print("1. Define image subsets for training and testing.")
             print("2. Display some images from each stack and stack dimensions for QC.")
             print("3. Train model.")
             print("4. Go back")
             selection3 = str(input("Select an option (type a number, press enter):\n"))
             if selection3=="1": #define image subsets for training and testing
+                print("***DEFINING IMAGE SUBSETS***")
                 #gridphase_train_slices_subset = input("Enter number of slice(s) for training the model, separated by "," only.")
                 gridphase_train_slices_subset = [45]
                 #gridphase_test_slices_subset = input("Enter number of slice(s) for testing of the model, separated by "," only.")
@@ -75,7 +83,7 @@ while selection != "7":
             elif selection3=="2": #plot some images and stack dimensions
                 ImgLoadProcess.displayImages_displayDims(gridrec_stack,phaserec_stack,label_stack,localthick_stack,gridphase_train_slices_subset,gridphase_test_slices_subset,label_train_slices_subset,label_test_slices_subset)
             elif selection3=="3": #train model
-                rf_transverse,FL_train,FL_test = ImgLoadProcess.train_model(gridrec_stack,phaserec_stack,label_stack,localthick_stack,gridphase_train_slices_subset,gridphase_test_slices_subset,label_train_slices_subset,label_test_slices_subset)
+                rf_transverse,FL_train,FL_test,Label_train,Label_test = ImgLoadProcess.train_model(gridrec_stack,phaserec_stack,label_stack,localthick_stack,gridphase_train_slices_subset,gridphase_test_slices_subset,label_train_slices_subset,label_test_slices_subset)
             elif selection3=="4": #go back one step
                 print("Going back one step...")
             else:
@@ -85,19 +93,44 @@ while selection != "7":
         hold = "1"
         print('Our OOB prediction of accuracy for is: {oob}%'.format(oob=rf_transverse.oob_score_ * 100))
         print("Would you like to print feature layer importance?")
-        hold = str(input("Enter 1 for yes, or 2 for no."))
+        hold = str(input("Enter 1 for yes, or 2 for no.\n"))
         if hold == "1":
-            ImgLoadProcess.print_feature_layers(rf_transverse,FL_train,FL_test)
+            ImgLoadProcess.print_feature_layers(rf_transverse)
         else:
             print("Okay. Going back.")
     elif selection=="4": #predict single slices
-        print("You selected option 4")
-        #class_prediction, class_prediction_prob = ImgLoadProcess.predict_testset(rf_transverse,FL_train,FL_test)
+        #print("You selected option 4")
+        selection4="1"
+        while selection4 != "4":
+            print("********_____SINGLE SLICE PREDICTIONS MENU_____********")
+            print("1. Predict single slices from test dataset.")
+            print("2. Generate confusion matrices.")
+            print("3. Plot images.")
+            print("4. Go back")
+            selection4 = str(input("Select an option (type a number, press enter):\n"))
+            if selection4=="1": #predict single slices from test dataset
+                class_prediction, class_prediction_prob = ImgLoadProcess.predict_testset(rf_transverse,FL_test)
+            elif selection4=="2": #generate confusion matrices
+                print("This step needs updating!")
+                print("figure out why matrices aren't actually printing")
+                print("Confusion Matrix")
+                ImgLoadProcess.make_conf_matrix(Label_test,class_prediction)
+                print("Normalized Confusion Matrix")
+                ImgLoadProcess.make_normconf_matrix(Label_test,class_prediction)
+            elif selection4=="3": #plot images
+                print("You selected option 3")
+                print("This step needs updating!")
+            elif selection4=="4": #go back one step
+                print("Going back one step...")
+            else:
+                print("Not a valid choice.")
     elif selection=="5": #predict all slices in 3d stack
         print("You selected option 5")
+        print("This step needs updating!")
     elif selection=="6": #performance metrics
         print("You selected option 6")
+        print("This step needs updating!")
     elif selection=="7":
-        print("Session ended")
+        print("Session ended.")
     else:
         print("Not a valid choice.")
